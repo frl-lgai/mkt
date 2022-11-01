55060a7918cf8aef496c3c2d248bf9f4ea53a3ef
Dong-Hyun Lee <dhlee347@gmail.com>
2022-11-01 07:57:59 +0000

    add test script and remove pythonpath

--- scripts/finetune_lingvo_test.sh
+++ scripts/finetune_lingvo_test.sh
@@ -7,14 +7,14 @@ export CUDA_HOME='/usr/local/cuda'
 export TOKENIZERS_PARALLELISM=false
 
 deepspeed \
-    --include localhost:0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
+    --include localhost:0,1,2,3 \
     --master_port $port \
     finetune_lingvo.py \
     --deepspeed \
     --deepspeed_config "deepspeed_configs/stage3.json" \
-    --model_dir "/w/exaone_2022/model_8.8B_BI_MT_02" \
+    --model_dir "/w/exaone_2022/model_1.7B_BI_MT_02" \
     --data_dir "/w/mkt/data/kobaco" \
-    --output_dir "/w/exp/mkt/model_8.8B_BI_MT_02" \
+    --output_dir "/w/exp/mkt/model_1.7B_BI_MT_02" \
     --num_epochs 20 \
     --per_device_batch_size 16 \
     --learning_rate 4e-5 \
