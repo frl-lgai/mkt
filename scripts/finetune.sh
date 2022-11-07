5483e9f1ee36dfe628d66efb8c0061c4eee2ea8b
Dong-Hyun Lee <dhlee347@gmail.com>
2022-11-07 02:16:40 +0000

    cleaning

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -7,4 +7,6 @@ export CUDA_HOME='/usr/local/cuda'
 export TOKENIZERS_PARALLELISM=false
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
+# $1 - GPUs to use (ex - 1,2,3)
+# $2 - config file (ex - configs/finetune_1.7B.yaml)
 deepspeed --include localhost:$1 --master_port $port finetune.py --deepspeed --config $2
